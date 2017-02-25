2091cc66d7cc31227f4011d109ab8ce469bc8a19
Lanckyblue <951587336@qq.com>
2017-02-25 16:21:10 +0800

    脚本预备工作完成

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 /**
  * Created by Chen-ACER on 2017/2/25.
  */
+requirejs.config({
+    baseUrl: '/',
+    paths: {
+
+        //第三方库的路径配置
+        jquery: 'lib/jquery/jquery.min',
+        bootstrap: 'lib/bootstrap/js/bootstrap.min',
+    },
+
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        }
+    }
+});
+
+//所有页面都需要这两个js，先加载他们
+require(['jquery', 'bootstrap']);
+
