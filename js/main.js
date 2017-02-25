bd58514511f049414b29f825d9574be4050fd6f8
Lanckyblue <951587336@qq.com>
2017-02-25 20:17:12 +0800

    初期预备完成，准备脚本开发

--- js/main.js
+++ js/main.js
@@ -8,6 +8,29 @@ requirejs.config({
         //第三方库的路径配置
         jquery: 'lib/jquery/jquery.min',
         bootstrap: 'lib/bootstrap/js/bootstrap.min',
+
+        // 自己写的路径配置
+        userList: 'js/user/list',
+        userProfile: 'js/user/profile',
+
+        teacherList: 'js/teacher/list',
+        teacherAdd: 'js/teacher/add',
+
+        homeLogin: 'js/home/login',
+        homeRepass: 'js/home/repass',
+        homeSettings: 'js/home/settings',
+
+        courseAdd: 'js/course/add',
+        courseAddStep1: 'js/course/add_step1',
+        courseAddStep2: 'js/course/add_step2',
+        courseAddStep3: 'js/course/add_step3',
+        courseCategory: 'js/course/category',
+        courseCategoryAdd: 'js/course/category_add',
+        courseList: 'js/course/list',
+        courseTopic: 'js/course/topic',
+
+        advertList: 'js/course/list',
+        advertAdd: 'js/course/add',
     },
 
     shim: {
@@ -20,3 +43,58 @@ requirejs.config({
 //所有页面都需要这两个js，先加载他们
 require(['jquery', 'bootstrap']);
 
+// 这里获取页面的pathname，然后对应的加载js
+(function(window){
+
+    var pathname = window.location.pathname;
+    switch (pathname){
+        case '/html/user/list.html':
+            require(['userList']);
+            break;
+        case '/html/user/profile.html':
+            require(['userProfile']);
+            break;
+
+        case '/html/teacher/list.html':
+            require(['teacherList']);
+            break;
+        case '/html/teacher/add.html':
+            require(['teacherAdd']);
+            break;
+
+        case '/html/home/login.html':
+            require(['homeLogin']);
+            break;
+        case '/html/home/repass.html':
+            require(['homeRepass']);
+            break;
+        case '/html/home/settings.html':
+            require(['homeSettings']);
+            break;
+
+        case '/html/course/add.html':
+            require(['courseAdd']);
+            break;
+        case '/html/course/add_step1.html':
+            require(['courseAddStep1']);
+            break;
+        case '/html/course/add_step2.html':
+            require(['courseAddStep2']);
+            break;
+        case '/html/course/add_step3.html':
+            require(['courseAddStep3']);
+            break;
+        case '/html/course/category.html':
+            require(['courseCategory']);
+            break;
+        case '/html/course/topic.html':
+            require(['courseTopic']);
+            break;
+        case '/html/advert/add.html':
+            require(['advertAdd']);
+            break;
+        case '/html/advert/list.html':
+            require(['advertList']);
+            break;
+    }
+})(window);
